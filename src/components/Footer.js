1bad735316800f13717b03cc82eb03cfd7ef5e8e
waseemrizi <waseemrizi@outlook.com>
2020-07-20 00:31:27 +0500

    footer js syling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,64 @@
-import React from 'react';
-import StickyFooter from 'react-sticky-footer';
+import React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import CallIcon from "@material-ui/icons/Call";
+const useStyles = makeStyles((theme) => ({
+  mainFooter: {
+    background: "#f0f1ec",
+    position: "relative",
+    bottom: 0,
+    marginTop: "80px",
+    color: "black",
+    textAlign: "center",
+  },
+  title: {
+    // textAlign: "center",
+  },
+  list: {
+    listStyle: "none",
+    marginLeft: "-25px",
+  },
+  spn: {
+    display: "block",
+    textAlign: "center",
+    // paddingTop: "5px",
+  },
+}));
 
-function Footer() {
-    return (
-    
-    <div>
-        <StickyFooter
-    bottomThreshold={50}
-    normalStyles={{
-    backgroundColor: "#3F51B5",
-    padding: "1.5rem"
-    }}
-    stickyStyles={{
-    backgroundColor: "rgba(255,255,255,.8)",
-    padding: "2rem"
-    }}
->
-    Developed By Waseem
-</StickyFooter>
-
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.mainFooter}>
+      <Grid container>
+        <Grid item xs={6}>
+          <h4 className={classes.title}>Shoe Store</h4>
+          <ul className={classes.list}>
+            <li>
+              <LocationOnIcon fontSize="small" />
+              Karachi, Pakistan
+            </li>
+            <li> Street 000, Shop# 0</li>
+          </ul>
+        </Grid>
+        <Grid item xs={6}>
+          <h4 className={classes.title}>Contacts</h4>
+          <ul className={classes.list}>
+            <li>
+              <WhatsAppIcon fontSize="small" /> 03001234567
+            </li>
+            <li>
+              <CallIcon fontSize="small" /> 021-111111-
+            </li>
+          </ul>
+        </Grid>
+      </Grid>
+      <hr />
+      <span className={classes.spn}>
+        &copy;{new Date().getFullYear()} Devleoped By Waseem 
+      </span>
     </div>
-    
-    
-    );
-}
+  );
+};
 
 export default Footer;
